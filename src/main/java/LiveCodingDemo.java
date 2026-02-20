c7072ddd6eb5a7fa199a76a8fca38202f95da718
Anastasiya Sycheva <a.a.sycheva95@yandex.ru>
2026-02-20 17:46:37 +0300

    поправила отступы

--- src/main/java/LiveCodingDemo.java
+++ src/main/java/LiveCodingDemo.java
@@ -8,16 +8,16 @@ public class LiveCodingDemo {
   public static int findMax(Integer[] numbers) {
 
     if (numbers == null || numbers.length == 0) {
-        return Integer.MIN_VALUE;
-      }
+      return Integer.MIN_VALUE;
+    }
 
     int maxValue = Integer.MIN_VALUE;
 
     for (Integer number : numbers) {
 
       if (number != null && number > maxValue) {
-          maxValue = number;
-        }
+        maxValue = number;
+      }
 
     }
     return maxValue;
