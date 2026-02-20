bee71b1f2cf853ae52930a532ef6ffc06a332929
Anastasiya Sycheva <a.a.sycheva95@yandex.ru>
2026-02-20 17:41:28 +0300

    документация: добавить результаты GATE-1 и обратную связь

--- src/main/java/LiveCodingDemo.java
+++ src/main/java/LiveCodingDemo.java
@@ -1,55 +1,26 @@
 public class LiveCodingDemo {
+
   public static void main(String [] args) {
-    findMax(new int[]{42});
+
+    System.out.println(findMax(new Integer[] {null, 1, 2, 3, 0}));
   }
 
-  static void printFizzBuzz(int n) {
+  public static int findMax(Integer[] numbers) {
 
-    for (int i = 1; i <= n; i++) {
-      if (i % 3 == 0 && i % 5 == 0) {
-        System.out.println("fizzBuzz");
-      } else if (i % 3 == 0) {
-        System.out.println("fizz");
-      } else if (i % 5 == 0) {
-        System.out.println(("Buzz"));
-      } else {
-        System.out.println(i);
-      }
-    }
-  }
-
-  static void sumEven(int[] numbers) {
-
-    int sum = 0;
-
-    for (int number : numbers) {
-
-      if (number % 2 == 0) {
-        sum += number;
+    if (numbers == null || numbers.length == 0) {
+        return Integer.MIN_VALUE;
       }
 
+    int maxValue = Integer.MIN_VALUE;
+
+    for (Integer number : numbers) {
+
+      if (number != null && number > maxValue) {
+          maxValue = number;
+        }
+
     }
-
-    System.out.println(sum);
-
+    return maxValue;
   }
 
-  static void findMax(int[] numbers) {
-
-    if (numbers.length == 0 || numbers == null) {
-
-      throw new IllegalArgumentException("List of numbers is empty!");
-    }
-
-    int maxValue = numbers[0];
-
-    for (int number : numbers) {
-      if (number > maxValue) {
-
-        maxValue = number;
-
-      }
-    }
-    System.out.println(maxValue);
-  }
 }
